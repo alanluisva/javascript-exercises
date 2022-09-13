18657e688dd6510a6fc4d936521d31b79dac0f41
alanluis <alan.luisva@gmail.com>
2022-09-13 16:50:05 -0500

    Split string reverse it and join it back together

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = string.split("")
+    let finalString = "";
 
+    for (let i = array.length - 1 ; i >= 0; i--) {
+        finalString += array[i]
+    } return finalString
 };
 
 // Do not edit below this line
