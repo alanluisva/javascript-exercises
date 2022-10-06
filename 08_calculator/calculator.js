b2e9169dfa6dfe9700d94b23fb7bee61533cc4b4
alanluis <alan.luisva@gmail.com>
2022-10-06 18:57:56 -0500

    Write the body of the functions add and subtract

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function () {
 
 };
 
-const power = function() {
-	
+const multiply = function () {
+
 };
 
-const factorial = function() {
-	
+const power = function () {
+
+};
+
+const factorial = function () {
+
 };
 
 // Do not edit below this line
