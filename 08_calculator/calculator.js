7eb9d18e43b791fde7c87117424bf5fb80906afa
alanluis <alan.luisva@gmail.com>
2022-10-06 20:31:45 -0500

    Write the body of the factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,29 +1,37 @@
 const add = function (num1, num2) {
-  return num1 + num2
+  return num1 + num2;
 };
 
 const subtract = function (num1, num2) {
-  return num1 - num2
+  return num1 - num2;
 };
 
 const sum = function (array) {
   return array.reduce((total, num) => {
-    return total + num
-  },0)
+    return total + num;
+  }, 0);
 };
 
 const multiply = function (array) {
   return array.reduce((total, num) => {
-    return total * num
-  },1)
+    return total * num;
+  }, 1);
 };
 
 const power = function (num1, num2) {
-  return Math.pow(num1, num2)
+  return Math.pow(num1, num2);
 };
 
-const factorial = function () {
-
+const factorial = function (num) {
+  let answer = 1;
+  if (num === 0 || num === 1) {
+    return answer;
+  } else {
+    for (let i = num; i >= 1; i--) {
+      answer = answer * i;
+    }
+    return answer;
+  }
 };
 
 // Do not edit below this line
@@ -33,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
