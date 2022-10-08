7aabeef761906ccb9ca38e7b17d664740ed968cf
alanluis <alan.luisva@gmail.com>
2022-10-07 20:02:48 -0500

    make the function "palindromes" works with single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // for (let i = 0; i < string.length; i++) {
+    //     console.log(string[i])
+    // }
+    let backwardsString = "";
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        backwardsString += string[i];
+    }
+
+    if (string === backwardsString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
