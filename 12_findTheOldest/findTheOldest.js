0165c10799438b858a776c09720e49be3d1d9860
alanluis <alan.luisva@gmail.com>
2022-10-12 09:49:29 -0500

    Finds the oldest person if someone or oldest person is still living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    const oldest = array.sort((a, b) => {
+        let date = new Date();
+        let year = date.getFullYear();
 
+        if (a["yearOfDeath"] === undefined) {
+            a["yearOfDeath"] = year;
+        }
+        if (b["yearOfDeath"] === undefined) {
+            b["yearOfDeath"] = year;
+        }
+
+        let age1 = `${a["yearOfDeath"] - a["yearOfBirth"]}`;
+        let age2 = `${b["yearOfDeath"] - b["yearOfBirth"]}`;
+
+        if (age1 < age2) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
